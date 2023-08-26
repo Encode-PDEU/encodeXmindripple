153b9aca73643159269fcbc84fecebb591417889
DhyanShah22 <dhyanshah229@gmail.com>
2023-08-26 16:17:53 +0530

    CheckAnswer

---
+++ pages/api/riddle/checkanswer.js
@@ -0,0 +1,14 @@
+import { supabase } from "@/lib/supabaseClient"
+
+
+export default async function handler(req,res) {
+    if(req.method == "GET") {
+        console.log('Hello!')
+
+    
+//const { data, error } = await supabase
+//.from('Riddle')
+//.select('answer')
+
+  }
+}
