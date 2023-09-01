2ac6e1f2fa1a794a99ee2d99cdcf4efbcf5a734a
DhyanShah22 <dhyanshah229@gmail.com>
2023-09-01 10:50:04 +0530

    Testing

--- pages/api/riddle/checkanswer.js
+++ pages/api/riddle/checkanswer.js
@@ -5,6 +5,7 @@ export default async function handler(req,res) {
     if (req.method == "GET") {
     console.log(req.query)
     req.query.riddle_id = parseInt(req.query.riddle_id)
+    req.query.score = parseInt(req.query.score)
     console.log(typeof(req.query.riddle_id))
     
     const { data, error } = await supabase
@@ -23,13 +24,25 @@ export default async function handler(req,res) {
           user_email: req.query.email,
           question_id: req.query.riddle_id
         }
-
       )
           console.log('Correct answer')
+          console.log(typeof(req.query.score))
           if(error) {
             console.log(error)
             return res.status(500).json('Unable to add to table')
           }
+      const { score_data, score_error } = await supabase.rpc (
+         "incrementScore",
+            {
+              user_email: req.query.email,
+              score_gain: req.query.score
+            }
+          )
+            console.log('Score Added')
+            if(score_error) {
+              console.log(score_error)
+              return res.status(500).json('Unable to add to table')
+            }
       return res.status(200).json({msg: 'Sahi jawab 150 rupees.'})
     }
     else {
