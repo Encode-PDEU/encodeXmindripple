6156f85b01313e4e35a05a4c49fb43da3dafe2f3
DhyanShah22 <dhyanshah229@gmail.com>
2023-09-29 12:35:58 +0530

    Riddle Active

---
+++ pages/api/riddle/active.js
@@ -0,0 +1,41 @@
+import { supabase } from "@/lib/supabaseClient"
+
+
+export default async function handler(req,res) {
+    if( req.method == "PATCH") {
+        console.log(req.body)
+
+    const { id } = req.body; 
+
+    try {
+        // Retrieve the current 'active' status of the record
+        const { data: currentData, error } = await supabase
+          .from('Riddle') // Replace with your actual table name
+          .select('active')
+          .eq('riddle_id', id);
+    
+        if (error) {
+          throw error;
+        }
+    
+        // Toggle the 'active' status
+        const newActiveStatus = !currentData[0].active;
+    
+        // Update the 'active' column in your Supabase table
+        const { data, error: updateError } = await supabase
+          .from('Riddle') // Replace with your actual table name
+          .update({ active: newActiveStatus })
+          .eq('riddle_id', id);
+    
+        if (updateError) {
+          throw updateError;
+        }
+    
+        res.status(200).json({ message: 'Record updated successfully', data });
+      } catch (error) {
+        console.error('Error updating record:', error.message);
+        res.status(500).json({ message: 'Error updating record' });
+      }
+    }
+    };
+    
