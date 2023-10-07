c97cc38a50ef927b6d6ce2a9ef1b8d2a895109d8
Preet Sojitra <preet.dev373@gmail.com>
2023-10-07 18:32:54 +0530

    fix: admin login and signup css

--- pages/admin/signup.js
+++ pages/admin/signup.js
@@ -1,11 +1,10 @@
-import React, { useState, useEffect, CSSProperties } from "react"
+import Link from "next/link"
+import axios from "axios"
+import { ReactMatrixAnimation } from "react-matrix-animation"
+import React, { useState, useEffect } from "react"
 import { useRouter } from "next/router"
 import Logos from "@/components/logos"
-// import Layout from "../app/layout"
-import { ReactMatrixAnimation } from "react-matrix-animation"
 import { Oswald, Roboto } from "next/font/google"
-import axios from "axios"
-import Link from "next/link"
 import { toast, ToastContainer } from "react-toastify"
 import "react-toastify/dist/ReactToastify.css"
 import { MoonLoader } from "react-spinners"
@@ -21,82 +20,83 @@ const roboto = Roboto({
   weight: ["400", "500", "700"],
 })
 
-const API_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:3000/api"
+const API_URL = process.env.NEXT_PUBLIC_API_URL
 
-const Login = () => {
+const Signup = () => {
   const router = useRouter()
   const [isMobile, setIsMobile] = useState(false)
 
-  // Check if the window is resized
-  // useEffect(() => {
-  //   // Check if the window width is less than a certain value (e.g., 768 for mobile)
-  //   const checkIsMobile = () => {
-  //     setIsMobile(window.innerWidth < 748)
-  //   }
+  // For checking if the window is resized
+  useEffect(() => {
+    // Check if user has already logged in
+    if (localStorage.getItem("admin_token")) {
+      router.replace("/admin/questions/all")
+    }
 
-  //   // Initial check
-  //   checkIsMobile()
+    // Check if the window width is less than a certain value (e.g., 768 for mobile)
+    // const checkIsMobile = () => {
+    //   setIsMobile(window.innerWidth < 748)
+    // }
 
-  //   // Add a listener for window resize
-  //   window.addEventListener("resize", checkIsMobile)
+    // // Initial check
+    // checkIsMobile()
 
-  //   // Clean up the listener when the component is unmounted
-  //   return () => {
-  //     window.removeEventListener("resize", checkIsMobile)
-  //   }
-  // }, [])
+    // // Add a listener for window resize
+    // window.addEventListener("resize", checkIsMobile)
+
+    // // Clean up the listener when the component is unmounted
+    // return () => {
+    //   window.removeEventListener("resize", checkIsMobile)
+    // }
+  }, [])
 
   const [email, setEmail] = useState("")
   const [password, setPassword] = useState("")
 
   const [isLoading, setIsLoading] = useState(false)
 
-  const handleLogin = (event) => {
+  const handleSignup = (event) => {
     event.preventDefault()
-    console.log("clicked")
 
     if (email == "" || password == "") {
       toast.error("Please enter all the fields", {
         position: "top-center",
+        autoClose: 2000,
       })
       return
     }
+    // console.log(email == "mindripple@gmail.com")
 
-    setIsLoading(true)
-    axios
-      .post(`${API_URL}/admin/login`, { email: email, password: password })
-      .then((res) => {
-        console.log(res.data)
-        localStorage.setItem("admin_token", JSON.stringify(res.data))
-        toast.success("Logged in Successfully!!", {
-          position: "bottom-right",
+    if (email == "encode_pdeu@gmail.com" || email == "mindripple@gmail.com") {
+      setIsLoading(true)
+      axios
+        .post(`${API_URL}/admin/signup`, { email: email, password: password })
+        .then((res) => {
+          console.log(res.data)
+          localStorage.setItem("admin_token", JSON.stringify(res.data))
+          toast.success("Signup  Successfull !!", {
+            position: "bottom-right",
+            // autoClose: 2000,
+          })
+          router.replace("/admin/questions/all")
         })
-
-        router.replace("/admin/questions/all") // Use replace instead of push so that the user can't go back to the login page
-      })
-      .catch((err) => {
-        console.log("Browser login error")
-        console.log(err)
-
-        // If the error is 401, then the user entered the wrong credentials
-        if (err.response?.status == 401) {
-          toast.error("Invalid credentials. Try again", {
-            position: "top-center",
+        .catch((err) => {
+          console.log(err)
+          toast.error("Signup failed. Try again.", {
+            position: "bottom-right",
           })
-          return
-        } else if (err.response?.status == 500) {
-          // If the error is 500, then there was an error with the server
-          toast.error("Unable to log in. Try again", {
-            position: "top-center",
-          })
-          return
-        }
+        })
+        .finally(() => {
+          setIsLoading(false)
+        })
+    } else {
+      toast.error("You are not allowed to login", {
+        position: "top-center",
+        autoClose: 2000,
       })
-      .finally(() => {
-        setIsLoading(false)
-      })
+      return
+    }
   }
-
   return (
     <>
       <div
@@ -135,7 +135,7 @@ const Login = () => {
               <h1
                 className={`${oswald.className} text-custom-green text-center font-semibold justify-center items-center md:justify-center md:items-center text-5xl mb-4 mt-6 p-2 md:text-7xl lg:text-6xl`}
               >
-                LOGIN
+                SIGNUP
               </h1>
               <form className="flex flex-col w-full mt-6 sm:px-8 md:px-14">
                 <label
@@ -146,8 +146,8 @@ const Login = () => {
                 </label>
                 <input
                   className="border rounded-lg w-full py-2 px-3 text-custom-grey bg-opacity-0 bg-black mb-5 md:text-2xl mt-1 placeholder:text-custom-dark-grey border-custom-dark-grey
-                  focus:outline-none focus:ring-2 focus:ring-custom-green focus:border-transparent
-                  "
+                focus:outline-none focus:ring-2 focus:ring-custom-green focus:border-transparent
+                "
                   id="Email"
                   placeholder="Enter your Email"
                   value={email}
@@ -163,8 +163,8 @@ const Login = () => {
                 </label>
                 <input
                   className="border rounded-lg md:mt-2 w-full py-2 px-3 text-custom-grey bg-opacity-0 bg-black p-4 mb-5 mt-1 md:text-2xl placeholder:text-custom-dark-grey border-custom-dark-grey
-                  focus:outline-none focus:ring-2 focus:ring-custom-green focus:border-transparent
-                  "
+                focus:outline-none focus:ring-2 focus:ring-custom-green focus:border-transparent
+                "
                   id="password"
                   type="password"
                   placeholder="Enter Password"
@@ -174,14 +174,14 @@ const Login = () => {
                 />
                 <div className="flex flex-row items-center justify-between">
                   <div className="text-custom-grey md:text-xl">
-                    <p>Do not have an account?</p>
+                    <p>Already have an account?</p>
                   </div>
                   <div>
                     <Link
-                      href="/admin/signup"
+                      href="/admin/login"
                       className=" align-baseline font-normal md:text-xl text-lg text-custom-grey underline"
                     >
-                      Sign up
+                      Login
                     </Link>
                   </div>
                 </div>
@@ -189,18 +189,18 @@ const Login = () => {
                   <button
                     type="submit"
                     className={`inline-block align-baseline font-semibold text-2xl bg-custom-yellow w-[350px] h-[50px] rounded py-2 px-4 text-black md:text-3xl
-                      ${
-                        isLoading
-                          ? "opacity-50 cursor-not-allowed"
-                          : "hover:bg-yellow-600"
-                      }
-                      `}
-                    onClick={handleLogin}
+                    ${
+                      isLoading
+                        ? "opacity-50 cursor-not-allowed"
+                        : "hover:bg-yellow-600"
+                    }
+                    `}
+                    onClick={handleSignup}
                     disabled={isLoading}
                   >
                     {isLoading ? (
                       <div className="flex justify-center items-center">
-                        <p className="mr-2">Logging in</p>
+                        <p className="mr-2">Signing up</p>
                         <MoonLoader
                           size={20}
                           color={"#000000"}
@@ -208,7 +208,7 @@ const Login = () => {
                         />
                       </div>
                     ) : (
-                      "Login"
+                      "Signup"
                     )}
                   </button>
                 </div>
@@ -221,4 +221,4 @@ const Login = () => {
   )
 }
 
-export default Login
+export default Signup
